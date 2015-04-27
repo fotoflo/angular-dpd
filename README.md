4ae56318c13d3b2fd0f46cac6d3fe019d84a04eb
Andrei Alecu <andreialecu@users.noreply.github.com>
2015-04-27 16:03:36 +0300

    Refactoring to remove cache support (use $http cache instead). Also removed callback functions in favor of promises.

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ angular-dpd
 A plugin for Angular that allows for easy interaction with deployd.
 Usage is the same as the default dpd.js file, but requires configuration of the necessary collections.
 Extra 'save' helper function added that will POST if the object has no 'id' and will put if it does.
-Also includes a cache for your objects that have been retrieved for easy insertion into the DOM.
-This allows you to quickly wire up your angular app with deployd and keep all of your files static so the deployd api can spend it's time serving data instead of a dynamic .js file.
 
 Example usage
 ---------------------
@@ -21,18 +19,17 @@ app.value('dpdConfig', {
 	serverRoot: 'http://someotherserver.com/', // optional, defaults to same server
 	socketOptions: { reconnectionDelayMax: 3000 }, // optional socket io additional configuration
 	useSocketIo: true // optional, defaults to false
-	noCache: true // optional, defaults to false (false means that caching is enabled, true means it disabled)
 });
 
 
 app.controller('bodyController',function($scope, dpd){
 
-	dpd.categories.get();
+	dpd.categories.get().then(function(data) { }).catch(function(error) { });
 	
-	dpd.categories.get('414b9c5cc315485d');
+	dpd.categories.get('414b9c5cc315485d').then(function(data) { }).catch(function(error) { });
 	
 	// Example with an [advanced query](http://docs.deployd.com/docs/collections/reference/querying-collections.md#s-Advanced%20Queries-2035):
-	dpd.categories.get($sort: {name: 1}, $limit: 10, rightsLevel: {$gt:0}};
+	dpd.categories.get($sort: {name: 1}, $limit: 10, rightsLevel: {$gt:0}}.then(function(data) { });
 	
 	// Promises are supported:
 	dpd.categories.post({"value":"cat1","typeId":"987ad2e6d2bdaa9d"})
@@ -41,74 +38,15 @@ app.controller('bodyController',function($scope, dpd){
 		})
 		.error(function (err) {
 			// on error
-		})
-		.finally(function () {
-			// finally
 		});
 	
-	dpd.categories.put('414b9c5cc315485d',{"value":"cat123"});
+	dpd.categories.put('414b9c5cc315485d',{"value":"cat123"}).success(function(category){ }).error(function(error) { });
 	
-	dpd.categories.del('414b9c5cc315485d');
-	
-	// You can also use a callback instead of using promises if you prefer:
-	dpd.categories.save({"value":"save POST","typeId":"987ad2e6d2bdaa9d"},function(result){
-	
-		result.value = "save PUT";
-		
-		dpd.categories.save(result);
-	});
-
+	dpd.categories.del('414b9c5cc315485d').success(function(category){ });
+  
+  // login a user
+  dpd.users.exec('login', { username: 'user', password: 'pass' }).success(function(session) { }).error(function(err) { });
 });
-```
-
-Cache
----------------------
-
-Unless caching is disabled via configuration, the dpd object comes with a cache object that will persist after calling a get().
-
-```javascript	
-// will return every category that is currently in the cache
-dpd.categories.cache.all 
-
-// will return a specific category from the cache
-dpd.categories.cache.get('414b9c5cc315485d') 
-
-// will fetch a single category from the database and if it's in the cache, update the cached item.
-// If it's not in the cache it will be added.
-dpd.categories.get(id,function(result){ ... });
-
-// will add a new category to the the database and on success will add it to the cache
-dpd.categories.post({..},function(result){ ... });
-
-// will update a category in the the database and on success will update it in the cache 
-// If it's not in the cache it will be added.
-dpd.categories.put(id, {..},function(result){ ... });
-
-// will delete a category from the the database and on success will delete it from the cache
-dpd.categories.del(id,function(){ ... });
-
-// login a user
-dpd.users.exec('login', { username: 'user', password: 'pass' }).success(function(session) { }).error(function(err) { });
-```
-
-Here is an example where you can query objects from the table and immediately put them on the screen.
-
-```html
-<!DOCTYPE html>
-<html ng-app="myApp">
-	<head>
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular.min.js"></script>
-		<script type="text/javascript" src="angular-dpd/angular-dpd.js"></script>
-		<script type="text/javascript" src="app.js"></script>
-	</head>
-	<body ng-controller="bodyController">
-		<ul ng-init="dpd.categories.get()">
-			<li ng-repeat="c in dpd.categories.cache.all">
-				{{c.value}}
-			</li>
-		</ul>
-	</body>
-</html>
 ```
 	
 Socket.IO
--- angular-dpd.js
+++ angular-dpd.js
@@ -1,205 +1,169 @@
-"use strict";
-
-var angularDpdSockets = [];
-
-angular.module('dpd', []).value('dpdConfig', [])
-    .factory('dpdSocket', function($rootScope, dpdConfig) {
-        if (!dpdConfig.useSocketIo) {
-            return undefined;
-        }
-        if (!io.connect) {
-            throw ('angular-dpd: socket.io library not available, includ the client library or set dpdConfig.useSocketIo = false');
-        }
-        var socket = angularDpdSockets[dpdConfig.serverRoot] = angularDpdSockets[dpdConfig.serverRoot] || io.connect(dpdConfig.serverRoot, dpdConfig.socketOpts);
-        var listeners = {};
-        return {
-            on: function(eventName, callback) {
-                listeners[callback] = function() {
-                    var args = arguments;
-                    $rootScope.$apply(function() {
-                        callback.apply(socket, args);
-                    });
-                };
-                socket.on(eventName, listeners[callback]);
-            },
-            emit: function(eventName, data, callback) {
-                socket.emit(eventName, data, function() {
-                    var args = arguments;
-                    $rootScope.$apply(function() {
-                        if (callback) {
-                            callback.apply(socket, args);
-                        }
-                    });
-                });
-            }, 
-            removeListener: function(eventName, f){
-                socket.removeListener(eventName, listeners[f]);
-                delete listeners[f]
-            },
-            rawSocket: socket
+(function () {
+  "use strict";
+  
+  var angularDpdSockets = [];
+  
+  angular.module('dpd', []).value('dpdConfig', [])
+    .factory('dpdSocket', ['$rootScope', 'dpdConfig', function ($rootScope, dpdConfig) {
+      if (!dpdConfig.useSocketIo) {
+        return undefined;
+      }
+      if (!io.connect) {
+        throw ('angular-dpd: socket.io library not available, include the client library or set dpdConfig.useSocketIo = false');
+      }
+      var socket = angularDpdSockets[dpdConfig.serverRoot] = angularDpdSockets[dpdConfig.serverRoot] || io.connect(dpdConfig.serverRoot, dpdConfig.socketOpts);
+      var listeners = {};
+      return {
+        on: function (eventName, callback) {
+          listeners[callback] = function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              callback.apply(socket, args);
+            });
+          };
+          socket.on(eventName, listeners[callback]);
+        },
+        emit: function (eventName, data, callback) {
+          socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (callback) {
+                callback.apply(socket, args);
+              }
+            });
+          });
+        }, 
+        removeListener: function (eventName, f) {
+          socket.removeListener(eventName, listeners[f]);
+          delete listeners[f];
+        },
+        rawSocket: socket
+      };
+    }])
+    .factory('dpd', ['$http', '$rootScope', 'dpdConfig', 'dpdSocket', function ($http, $rootScope, dpdConfig, dpdSocket) {
+      var dpd = {};
+      dpd.errors = [];
+      dpd.socket = dpdSocket;
+      if (angular.isArray(dpdConfig)) {
+        dpdConfig = {
+          collections: dpdConfig
         };
-    })
-    .factory('dpd', function($http, $rootScope, dpdConfig, dpdSocket) {
-        var dpd = {};
-        dpd.errors = [];
-        dpd.socket = dpdSocket;
-        if (angular.isArray(dpdConfig)) {
-            dpdConfig = {
-                collections: dpdConfig
-            };
-        }
-
-        var serverRoot = dpdConfig.serverRoot.replace(/\/$/, "") || "";
-
-        if (!Array.prototype.forEach) {
-            Array.prototype.forEach = function(fn, scope) {
-                for (var i = 0, len = this.length; i < len; ++i) {
-                    fn.call(scope, this[i], i, this);
-                }
+      }
+      
+      var serverRoot = dpdConfig.serverRoot.replace(/\/$/, "") || "";
+      
+      if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (fn, scope) {
+          for (var i = 0, len = this.length; i < len; ++i) {
+            fn.call(scope, this[i], i, this);
+          }
+        };
+      }
+      
+      var ef = function (data, status, headers, config) {
+        dpd.errors.push(data);
+      };
+      
+      var isComplexQuery = function (obj) {
+        if (obj) {
+          for (var k in obj) {
+            if (obj.hasOwnProperty(k)) {
+              if (typeof obj[k] === 'object') {
+                return true;
+              }
             }
+          }
         }
+        return false;
+      };
+      
+      dpdConfig.collections.forEach(function (collection) {
+        dpd[collection] = {};
 
-        var ef = function(data, status, headers, config) {
-            dpd.errors.push(data);
-        }
-
-        var checkUndefinedFunc = function(f) {
-            if (typeof f == 'undefined')
-                f = function() {};
-
-            return f;
-        }
-
-        var isComplexQuery = function(obj) {
-            if (obj) {
-                for (var k in obj) {
-                    if (obj.hasOwnProperty(k)) {
-                        if (typeof obj[k] === 'object') {
-                            return true;
-                        }
-                    }
-                }
+        dpd[collection].get = function (query, options) {
+          options = angular.extend({ withCredentials: true }, options);
+          if (typeof query == "string") {
+            return $http.get(serverRoot + '/' + collection + '/' + query, options).error(ef);
+          } else {
+            if (typeof query == "undefined") {
+              return $http.get(serverRoot + '/' + collection, options).error(ef);
+            } else {
+              if (isComplexQuery(query)) {
+                var query = encodeURI(JSON.stringify(query));
+                return $http.get(serverRoot + '/' + collection + '?' + query, options).error(ef);
+              } else {
+                return $http.get(serverRoot + '/' + collection, {
+                  params: query,
+                  withCredentials: true
+                }).error(ef);
+              }
             }
-            return false;
-        }
-
-        dpdConfig.collections.forEach(function(d) {
-            dpd[d] = {};
-            if (!dpdConfig.noCache) {
-                dpd[d].cache = {
-                    all: [],
-                    get: function(id) {
-                        for (var i in dpd[d].cache.all) {
-                            if (dpd[d].cache.all[i].id == id)
-                                return dpd[d].cache.all[i];
-                        }
-                    }
-                };
-            }
-            dpd[d].get = function(o, s, e) {
-                if (typeof o == "string") {
-                    return $http.get(serverRoot + '/' + d + '/' + o, { withCredentials: true }).success(function(data, status, headers, config) {
-                        if (!dpd[d].cache) return;
-                        var add = true;
-                        for (var i in dpd[d].cache.all) {
-                            if (dpd[d].cache.all[i].id == o) {
-                                add = false;
-                                dpd[d].cache.all[i] = data;
-                            }
-                        }
-                        if (add)
-                            dpd[d].cache.all.push(data);
-                    }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                } else {
-                    if (typeof o == "function") {
-                        e = s;
-                        s = o;
-                        return $http.get(serverRoot + '/' + d, { withCredentials: true }).success(function(data, status, headers, config) {
-                            if (!dpd[d].cache) return;
-                            dpd[d].cache.all = data;
-                        }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                    } else {
-                        if (isComplexQuery(o)) {
-                            var query = encodeURI(JSON.stringify(o));
-                            return $http.get(serverRoot + '/' + d + '?' + query, { withCredentials: true }).success(function(data, status, headers, config) {
-                                if (!dpd[d].cache) return;
-                                dpd[d].cache.all = data;
-                            }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                        } else {
-                            return $http.get(serverRoot + '/' + d, {
-                                params: o,
-                                withCredentials: true
-                            }).success(function(data, status, headers, config) {
-                                if (!dpd[d].cache) return;
-                                dpd[d].cache.all = data;
-                            }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                        }
-                    }
-                }
-            };
-
-            dpd[d].put = function(id, o, s, e) {
-                return $http.put(serverRoot + '/' + d + '/' + id, o, { withCredentials: true }).success(function(data, status, headers, config) {
-                    if (!dpd[d].cache) return;
-                    var add = true;
-                    for (var i in dpd[d].cache.all) {
-                        if (dpd[d].cache.all[i].id == id) {
-                            add = false;
-                            dpd[d].cache.all[i] = data;
-                        }
-                    }
-                    if (add)
-                        dpd[d].cache.all.push(data);
-                }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-            };
-
-            dpd[d].post = function(o, s, e) {
-                return $http.post(serverRoot + '/' + d + '/', o, { withCredentials: true }).success(function(data, status, headers, config) {
-                    if (!dpd[d].cache) return;
-                    dpd[d].cache.all.push(data);
-                }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-            };
-
-            dpd[d].del = function(id, s, e) {
-                return $http.delete(serverRoot + '/' + d + '/' + id, { withCredentials: true }).success(function(data, status, headers, config) {
-                    if (!dpd[d].cache) return;
-                    for (var i in dpd[d].cache.all) {
-                        if (dpd[d].cache.all[i].id == id) {
-                            dpd[d].cache.all.splice(i, 1);
-                        }
-                    }
-                }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-            };
-
-            dpd[d].save = function(obj, s, e) {
-                if (typeof obj.id == 'string') {
-                    dpd[d].put(obj.id, obj, s, e);
-                } else {
-                    dpd[d].post(obj, s, e);
-                }
-            };
-            
-            dpd[d].exec = function (func, o, s, e) {
-                return $http.post(serverRoot + '/' + d + '/' + func, o, { withCredentials: true }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));                
-            }
-
-            dpd[d].on = function(scope, event, f) {
-                if (!dpdSocket) return;
-                dpdSocket.on(d + ':' + event, f);
-                scope.$on("$destroy", function(){
-                    dpdSocket.removeListener(d + ':' + event, f);
-                })
-            };
-        });
-
-        dpd.on = function(scope, event, f) {
-            if (!dpdSocket) return;
-            dpdSocket.on(event, f);
-            scope.$on("$destroy", function(){
-                dpdSocket.removeListener(event, f);
-            })
+          }
         };
         
-        $rootScope.dpd = dpd;
-        return dpd;
-    });
+        dpd[collection].put = function (id, data, options) {
+          options = angular.extend({ withCredentials: true }, options);
+          return $http.put(serverRoot + '/' + collection + '/' + id, data, options).error(ef);
+        };
+        
+        dpd[collection].post = function (data, options) {
+          options = angular.extend({ withCredentials: true }, options);
+          return $http.post(serverRoot + '/' + collection + '/', data, options).error(ef);
+        };
+        
+        dpd[collection].del = function (id, options) {
+          options = angular.extend({ withCredentials: true }, options);
+          return $http.delete(serverRoot + '/' + collection + '/' + id, options).error(ef);
+        };
+        
+        dpd[collection].save = function (obj, options) {
+          options = angular.extend({ withCredentials: true }, options);
+          if (typeof obj.id == 'string') {
+            dpd[collection].put(obj.id, obj, options);
+          } else {
+            dpd[collection].post(obj, options);
+          }
+        };
+        
+        dpd[collection].exec = function (funcName, data, options) {
+          var options = angular.extend({
+            method: "POST",
+            url: serverRoot + '/' + collection + '/' + funcName,
+            withCredentials: true,
+          }, options);
+
+          if (/(POST|PUT)/i.test(options.method)) {
+            options.data = data;
+          } else {
+            options.params = data;
+          }
+          
+          return $http(options).error(ef);
+        };
+        
+        dpd[collection].on = function (scope, event, f) {
+          if (!dpdSocket) return;
+          if (typeof scope !== 'object' || !scope.$on) {
+            throw new Error("Invalid scope. Please pass an angular scope as the first parameter.");
+          }
+          dpdSocket.on(collection + ':' + event, f);
+          scope.$on("$destroy", function () {
+            dpdSocket.removeListener(collection + ':' + event, f);
+          });
+        };
+      });
+      
+      dpd.on = function (scope, event, f) {
+        if (!dpdSocket) return;
+        if (typeof scope !== 'object' || !scope.$on) {
+          throw new Error("Invalid scope. Please pass an angular scope as the first parameter.");
+        }
+        dpdSocket.on(event, f);
+        scope.$on("$destroy", function() {
+          dpdSocket.removeListener(event, f);
+        });
+      };
+      
+      return dpd;
+    }]);
+})();
